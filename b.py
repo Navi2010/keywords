58d37f621f95f0a9cfc03cb79d83baa740835f74
Navi2010 <99569813+Navi2010@users.noreply.github.com>
2024-09-23 16:52:35 -0500

    Add files via upload

---
+++ b.py
@@ -0,0 +1,8 @@
+word = input('enter a word: ')
+
+for i in word:
+    if i=="a":
+        print ('there is 1 or more a.')
+        break
+    else:
+        print('there are no a in your word.')
---
+++ untitled.py
@@ -0,0 +1,11 @@
+for i in range(10):
+    if i % 20==0:
+        print('twist')
+    elif i % 15==0:
+        pass
+    elif i % 5==0:
+        print('hello')
+    elif i % 3==0:
+        pass
+    else:
+        print(i)
